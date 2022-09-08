11b44de81390980e89ffcbc7a0ab2a52f34aa58c
Gian Lucas <lgian584@gmail.com>
2022-09-08 11:38:43 -0300

    add home animation

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,16 +1,16 @@
-import { HomeContainer } from "./styles";
+import { HomeContainer, InfoContainer } from "./styles";
 import programmingImg from "../../assets/programming.svg";
 import { RocketLaunch } from "phosphor-react";
 
 export function Home() {
   return (
     <HomeContainer>
-      <div>
+      <InfoContainer>
         <h1>
           <RocketLaunch weight="bold" /> GIAN LUCAS
         </h1>
         <span>{"<Desenvolvedor Front End Web />"}</span>
-      </div>
+      </InfoContainer>
 
       <img src={programmingImg} alt="" />
     </HomeContainer>
--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -6,26 +6,8 @@ export const HomeContainer = styled.section`
   justify-content: space-between;
   align-items: center;
 
-  div {
-    display: flex;
-    justify-content: flex-start;
-    flex-direction: column;
-    gap: 1.2rem;
-
-    h1 {
-      display: flex;
-      justify-content: flex-start;
-      align-items: center;
-      gap: 0.8rem;
-
-      font-size: 4.4rem;
-    }
-    span {
-      font-size: 2.4rem;
-    }
-  }
-
   img {
+    animation: img-animation 2s;
     width: 50%;
     min-width: 380px;
     max-width: 650px;
@@ -33,9 +15,62 @@ export const HomeContainer = styled.section`
 
   @media (max-width: 771px) {
     justify-content: center;
-
     img {
       display: none;
+    }
+  }
+  @keyframes img-animation {
+    0% {
+      opacity: 0;
+      transform: translateX(-20rem);
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  gap: 1.2rem;
+
+  h1 {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 0.8rem;
+
+    font-size: 4.4rem;
+
+    animation: h1-animation 2s;
+
+    @media (max-width: 771px) {
+      justify-content: center;
+    }
+  }
+  span {
+    animation: span-animation 2s;
+    font-size: 2.4rem;
+  }
+
+  @keyframes span-animation {
+    0% {
+      opacity: 0;
+      transform: translateY(-10rem);
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+  @keyframes h1-animation {
+    0% {
+      opacity: 0;
+      transform: translateY(10rem);
+    }
+    100% {
+      opacity: 1;
     }
   }
 `;
