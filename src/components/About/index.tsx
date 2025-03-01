f4c055d4a06577b4164a24410aacb179eae7df6b
gian <gian@pop-os.localdomain>
2025-03-01 17:45:09 -0300

    update about

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -14,10 +14,11 @@ export function About() {
       <div>
         <h1 data-aos="fade-down">Sobre mim</h1>
         <p data-aos="zoom-in">
-          Opa! Tudo bem? me chamo Gian Lucas, tenho 19 anos e sou apaixonado por
-          desenvolvimento web. Tenho mais afinidade com a stack
-          ReactJS + NextJS + NodeJS. Além disso, tenho uma base bem sólida em Java e Python. Fiz o curso técnico em informática integrado ao ensino médio
-          no IFPB e agora estou cursando Ciência da Computação na UEPB.
+          Opa! Tudo bem? me chamo Gian Lucas, tenho 20 anos e sou apaixonado por
+          desenvolvimento web. Tenho mais afinidade com a stack ReactJS + NextJS
+          + NodeJS. Além disso, tenho uma base bem sólida em Java e Python. Fiz
+          o curso técnico em informática integrado ao ensino médio no IFPB e
+          agora estou cursando Ciência da Computação na UEPB.
         </p>
 
         <LinkButton data-aos="fade-up" download href={GianCV} target="_blank">
