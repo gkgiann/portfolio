3ec2479dbf5aeb24a79dbaa4d0779456c386adcc
gian <lgian584@gmail.com>
2025-09-12 13:38:45 -0300

    feat: include all components in App: header, footer, and main content sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import Experiences from "./components/Experiences";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import ProjectsList from "./components/projects/ProjectsList";
+import SkillsList from "./components/skills/SkillsList";
+
 function App() {
-  return <h1 className="text-4xl font-bold">Gian Portfólio</h1>;
+  return (
+    <>
+      <Header />
+      <main className="px-6 md:px-12">
+        <Home />
+        <div className="max-w-5xl mx-auto">
+          <Experiences />
+          <SkillsList />
+          <ProjectsList />
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
